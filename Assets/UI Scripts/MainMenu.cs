ce6dc917b78119923e1f5f9d654514828b6429d1
Joshua Francis <joshua.francis@live.com.au>
2020-05-04 13:18:11 +1000

    Added setup for UI
    Added the Main Menu scene and some scripts to control everything, the
    script has not been added yet.

---
+++ Assets/UI Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button playButton = null;
+    public Button quitButton = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/UI Scripts/SceneController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+	private static SceneController instance = null;
+
+	public static SceneController Instance
+	{
+		get
+		{
+			if (instance = null)
+			{
+				instance = new SceneController();
+			}
+			return instance;
+		}
+	}
+
+	public string mainMenuScene;
+	public string gameScene;
+	public string winScreen;
+	private void Awake()
+	{
+		DontDestroyOnLoad(this.gameObject);
+	}
+	// Update is called once per frame
+	void Start()
+	{
+
+	}
+}
