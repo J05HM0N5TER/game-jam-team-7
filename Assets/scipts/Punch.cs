c856dd892c3ce1c9c6897b79a0320b43d9db74ab
Sean <seanclading@gmail.com>
2020-05-04 15:48:58 +1000

    player controller, not working fully just yet

---
+++ Assets/scipts/Punch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Punch : MonoBehaviour
+{
+    public int punchForce = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            float xRotation = -Vector3.Normalize(transform.parent.position - transform.position).x;
+            other.GetComponent<Rigidbody>().AddForce(new Vector3(xRotation * punchForce, 0, 0), ForceMode.Impulse);
+        }
+    }
+}
