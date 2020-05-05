9fd4cc86016244218da4e55728ed52392ebd1bd5
Sean <seanclading@gmail.com>
2020-05-05 12:08:51 +1000

    meteors
    made gamemanager that will spawn meteors around the x axis and point them at the player, and made the meteors delete when they hit something

--- Assets/scipts/GameManager.cs
+++ Assets/scipts/GameManager.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject meteorPrefab;
+    public float meteorSpawnDelay = 2.0f;
+    private bool canSpawnMeteor = true;
+    public float sizeOfSpawnRadius = 20.0f;
+    public float minDistanceFromCentr = 20.0f;
+    public float meteorSpeed = 5.0f;
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,31 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(canSpawnMeteor)
+        {
+            Vector3 randomPos = Random.insideUnitCircle * sizeOfSpawnRadius;
+            randomPos += randomPos.normalized * minDistanceFromCentr;
+            GameObject newMeteor =  Instantiate(meteorPrefab, randomPos, new Quaternion(0, 0, 0, 0));
+            newMeteor.gameObject.GetComponent<Rigidbody>().AddForce((player.transform.position - newMeteor.GetComponent<Transform>().position) * meteorSpeed, ForceMode.Impulse);
+            StartCoroutine(spawnMeteor());
+        }
         
+
     }
+    IEnumerator spawnMeteor()
+    {
+        canSpawnMeteor = false;
+        yield return new WaitForSeconds(meteorSpawnDelay);
+        canSpawnMeteor = true;
+
+    }
+    void playsounds()
+    {
+
+    }
+    void playanothersound()
+    {
+
+    }
+
 }
---
+++ Assets/scipts/Meteor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meteor : MonoBehaviour
+{
+    //this is to allow this script to call functions from gamemanager so can play sounds
+    public GameObject gamemanager;
+    private float lifetime = 10.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       //gamemanager.GetComponent<GameManager>();
+        Destroy(gameObject, lifetime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //if(game)
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
