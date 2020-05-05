44183dc21134b328a715545df20e738596ce40ae
Sean <seanclading@gmail.com>
2020-05-06 01:26:26 +1000

    added another animation and started the blackhole script

---
+++ Assets/scipts/BlackHole.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHole : MonoBehaviour
+{
+    private GameObject[] players;
+    public float blackHolePullSpeed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var player in players)
+        {
+            player.gameObject.GetComponent<Rigidbody>().AddForce((gameObject.transform.position - player.GetComponent<Transform>().position) * blackHolePullSpeed);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject collidedObject = collision.gameObject;
+       
+        if(collidedObject.CompareTag("player"))
+        {
+            Destroy(collidedObject);
+        }
+    }
+}
